351c25816d354c1900c913b96df174c01e5820f2
Mamat1411 <muhammadmujahid14@gmail.com>
2021-08-26 00:28:23 +0700

    Mengurutkan nilai sebelum menentukan nilai minimum dan maximum

--- nilai.php
+++ nilai.php
@@ -1,7 +1,20 @@
 <?php
 $nilai = "72 65 73 78 75 74 90 81 87 65 55 69 72 78 79 91 100 40 67 77 86";
+
+//Memecah string menjadi array
 $value = explode(" ", $nilai);
 for ($i=0; $i < count($value); $i++) { 
     echo $value[$i]." ";
+}
+
+//Menghitung rata-rata nilai array
+$average = array_sum($value)/count($value);
+echo "<br>Nilai rata-rata dari array diatas adalah ".$average;
+
+//Mengurutkan nilai array sebelum ditentukan nilai minimum dan maximum
+sort($value, SORT_NUMERIC);
+echo "<hr>Mengurutkan Nilai Array sebelum ditentukan nilai minimum dan maximum: ";
+for ($i=0; $i < count($value); $i++) { 
+    echo $value[$i]. " ";
 }
 ?>
