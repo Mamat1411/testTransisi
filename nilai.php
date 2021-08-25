7cf647cd45a0045bf39d90697bc3f3872e61edf7
Mamat1411 <muhammadmujahid14@gmail.com>
2021-08-26 00:01:52 +0700

    Menggunakan fungsi explode untuk memecah string menjadi array

--- nilai.php
+++ nilai.php
@@ -1,3 +1,7 @@
 <?php
 $nilai = "72 65 73 78 75 74 90 81 87 65 55 69 72 78 79 91 100 40 67 77 86";
+$value = explode(" ", $nilai);
+for ($i=0; $i < count($value); $i++) { 
+    echo $value[$i]." ";
+}
 ?>
