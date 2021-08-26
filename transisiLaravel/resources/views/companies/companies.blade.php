815b3ce0f78995de21646e8c4b9976499fa826a2
Mamat1411 <muhammadmujahid14@gmail.com>
2021-08-26 14:33:27 +0700

    Menambahkan file companies.blade.php

---
+++ transisiLaravel/resources/views/companies/companies.blade.php
@@ -0,0 +1,16 @@
+@extends('../layout/main')
+@section('title', 'Data Perusahaan')
+
+@section('container')
+<div class="container">
+    <div class="column">
+        <div class="row">
+            <h1 class="my-3">Data Perusahaan</h1>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <a href="/companies/create" class="btn btn-success">Tambahkan Data Perusahaan</a>
+</div>
+@endsection
